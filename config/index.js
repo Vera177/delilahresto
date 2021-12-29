343ad612549afba95064e19f1ff1942c0624a091
“Vera <veradeviaje@gmail.com>
2021-12-28 22:11:40 -0300

    db configuration

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
     database: {
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || 3306,
-        name: process.env.DB_NAME || 'delilah',
+        name: process.env.DB_NAME || 'delilah_resto',
         user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || ''
     }
--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.listen(config.port, () => {
-    console.log(`Server started on port: ${config.port}`);
+    console.log(`Server started on port: http://localhost:${config.port}`);
 });    
